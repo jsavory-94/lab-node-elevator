474810a668d00d710cf772d6c819ff262f2fc963
James Savory <james.pa.savory>
2018-01-30 07:19:20 +0100

    BCN - James Savory - Week4, Exercise 1

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -2,18 +2,31 @@ class Elevator {
   constructor(){
     this.floor      = 0;
     this.MAXFLOOR   = 10;
-    this.requests   = [];
+    this.requests   = [Person];
   }
 
-  start() { }
-  stop() { }
+  start() {
+    setInterval(update(), 1000);
+  }
+
+  stop() {clearInterval(start);}
+
   update() { }
   _passengersEnter() { }
+  
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  floorUp(floor) {if (this.floor < this.MAXFLOOR){
+    floor++;
+  } 
+ 
+
+  floorDown(() => {if (this.floor > 0){
+    floor--;}
+  });    
+
+  call()
+
+  log(() => {console.log("Direction: up | Floor: 0 ")});
 }
 
 module.exports = Elevator;
